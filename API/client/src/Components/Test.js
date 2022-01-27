9f725e31676350675980af113d4e275326f32ad4
danielmurphy1 <daniel.murphy.1@gmail.com>
2022-01-26 21:56:23 -0500

    started branch for front end design

---
+++ API/client/src/Components/Test.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Test() {
+    return (
+        <Container>
+            <h1>Hello World</h1>   
+        </Container>
+    );
+}
+
+export default Test;
