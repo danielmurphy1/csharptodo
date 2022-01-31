9e7156a57ad017f2043325cd18c33898fdf00452
danielmurphy1 <daniel.murphy.1@gmail.com>
2022-01-31 01:21:23 -0500

    added DatabaseConnection class

---
+++ Core/DatabaseConnection.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Core
+{
+    internal class DatabaseConnection
+    {
+        public NpgsqlConnection GetConnection()
+        {
+            string connString = "host=localhost;Username=postres;Password=pitdline;Database=test_todos;Port=5432";
+
+            NpgsqlConnection conn = new NpgsqlConnection(connString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
