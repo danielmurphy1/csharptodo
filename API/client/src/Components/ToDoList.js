ca7e54b64e30f8fe8596b27ba037c65cfed95298
danielmurphy1 <daniel.murphy.1@gmail.com>
2022-02-04 14:14:59 -0500

    line through for completed items and conditional delete button

--- API/client/src/Components/ToDoList.js
+++ API/client/src/Components/ToDoList.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import { ListGroup, InputGroup, Button, Container } from 'react-bootstrap';
+import { ListGroup, InputGroup, Button } from 'react-bootstrap';
 
 function ToDoList(props){
     console.log(props.todos)
     let todos = props.todos.map(todo =>{
         return <ListGroup.Item className="d-flex justify-content-between align-items-end" key={todo.id}>
                     <InputGroup.Checkbox checked={todo.isComplete} value={todo.isComplete} onChange={(e) => props.handleCheckboxClick(todo.id, e.target.checked)}/>
-                    <span className="mx-3 w-100">{todo.text}</span>
-                    <Button className="btn btn-danger">Delete</Button>
+                    <span className={`mx-3 w-100 ${todo.isComplete && 'line-through'}`} >{todo.text}</span>
+                    <Button className="btn btn-danger" disabled={!todo.isComplete}>Delete</Button>
                 </ListGroup.Item>
     })
 
     return(
-        // <ListGroup as="ul" variant="flush" className="w-50 mx-auto">
-        //     <ListGroup.Item className="d-flex justify-content-between align-items-end">
-        //         <InputGroup.Checkbox />
-        //         <span className="mx-3 w-100">Run</span>
-        //         <Button className="btn btn-danger">Delete</Button>
-        //     </ListGroup.Item>
-        //     <ListGroup.Item className="d-flex justify-content-between align-items-end">
-        //         <InputGroup.Checkbox />
-        //         <span className="mx-3 w-100">Go Food Shopping and then walk the dog around the block</span>
-        //         <Button className="btn btn-danger">Delete</Button>
-        //     </ListGroup.Item>
-        // </ListGroup>
         <ListGroup as="ul" variant="flush" className="w-50 mx-auto">
             {todos}
         </ListGroup>
