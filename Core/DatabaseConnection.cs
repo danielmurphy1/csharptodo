32bce1e73012d2fb81e6c4c948981cc9d6ec728b
danielmurphy1 <daniel.murphy.1@gmail.com>
2022-02-04 17:13:28 -0500

    attempt use DotNetEnv package

--- Core/DatabaseConnection.cs
+++ Core/DatabaseConnection.cs
@@ -5,21 +5,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DotNetEnv;
 
 
 namespace Core
 {
     public class DatabaseConnection
     {
-        //private readonly IConfiguration configuration;
-        //public DatabaseConnection(IConfiguration configuration)
-        //{
-        //    this.configuration = configuration;
-        //}
+        
         public NpgsqlConnection GetConnection()
         {
-            string connString = "host=localhost;Port=5432;Database=test_todos;Username=postgres;Password=pitdline";
-            //string connString = configuration.GetConnectionString("Default");
+            Env.TraversePath().Load();
+            //string connString = "host=localhost;Port=5432;Database=test_todos;Username=postgres;Password=pitdline";
+            string connString = System.Environment.GetEnvironmentVariable("SECRET");
 
             NpgsqlConnection conn = new NpgsqlConnection(connString);
             conn.Open();
